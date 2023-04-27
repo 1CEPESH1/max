a3a1344da4ebd244cad37a86be816be22e89fbed
pilot commit <vlad_pestov04@mail.ru>
2023-04-28 00:46:53 +0300

    commit four

--- main.cpp
+++ main.cpp
@@ -1,45 +1,33 @@
 #include <iostream>
 
 
-void max(int a, int b)
+int max(int a, int b)
 {
+    int maximum = b;
     if (a > b)
     {
-        std::cout << a;
+        maximum = a;
 
     }
-    else
-    {
-        std::cout << b;
+    std::cout << "function accepting 2 int values" << std::endl;
+    return maximum;
 
-    }
 }
 
-void max(float a, float b)
+int max(float a, float b)
 {
+    float maximum = b;
     if (a > b)
     {
-        std::cout << a;
+        maximum = a;
 
     }
-    else
-    {
-        std::cout << b;
+    std::cout << "function accepting 2 float values" << std::endl;
+    return maximum;
 
-    }
 }
 
-/*void max(int a, int b, int c)
-{
-    int* arr = new int [3]{a, b ,c};
-    int Maximum = arr[0];
-    for(int i = 1; i < 3; i ++){
-        if(arr[i] > Maximum){
-            Maximum = arr[i];
-        }
-    }
-    std::cout << Maximum;
-}*/
+
 
 int max(int a, int b, int c)
 {
@@ -52,23 +40,31 @@ int max(int a, int b, int c)
     {
         maximum = c;
     }
+    std::cout << "function accepting 3 int values" << std::endl;
     return maximum;
+
 }
 
 
 int main()
 {
-    float a = 0, b = 0, c = 0, d = 0;
-    std::cout << " how many numbers do you want to enter 2 or 3?" << std::endl;
+    float a = 3, b = 4, c = 5;
+
+    std::cout << max(a, b, c);
+    std::cout << std::endl;
+    std::cout << max(a, b);
+    std::cout << std::endl;
+    std::cout << max((int)a, (int)c);
+    std::cout << std::endl;
+    /*std::cout << " how many numbers do you want to enter 2 or 3?" << std::endl;
     std::cin >> d;
 
     if (d == 2)
     {
         std::cout << "enter two" << std::endl;
         std::cin >> a >> b;
-        max(a, b);
+        std::cout << max(a, b);
     }
-
 
     if (d == 3)
     {
@@ -76,5 +72,5 @@ int main()
         std::cin >> a >> b >> c;
         std::cout << max(a, b, c);
     }
-    return 0;
+    return 0;*/
 }
